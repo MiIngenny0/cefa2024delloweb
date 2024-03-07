bbd62f635034eeda6d32d0eb3de3192aa96179d1
sofiamariana <marianaossagutierrez07@gmail.com>
2024-03-07 14:34:39 -0500

    subiendo archivos

--- dellowebcefaMarcela2024/ejercicio calculadora vectores/ejercicio.js
+++ dellowebcefaMarcela2024/ejercicio calculadora vectores/ejercicio.js
@@ -2,31 +2,37 @@ function operacion(a, b){
     console.log("ingreso a la funcion de operaciones")
     var operador = document.getElementById("operador").value;
     console.log("el operador es;", operador)
+ switch (operador)
 
-    if (operador == "suma"){
+    {
+        case "suma":
+    
         num1= parseInt(a);
         num2= parseInt(b)
         suma=num1+num2;
         document.getElementById("resultSuma").innerHTML=suma
-    }
-    else (operador == "resta");{
+        break;
+    
+    case "resta":
         num1= parseInt(a);
         num2= parseInt(b)
         resta=num1-num2;
         document.getElementById("resultResta").innerHTML=resta
-    }
-    if (operador == "multiplicacion");{
+        break;
+    
+   case "multiplicacion":
         num1= parseInt(a);
         num2= parseInt(b)
         multiplicacion=num1*num2;
         document.getElementById("resultMult").innerHTML=multiplicacion
-    }
-    if (operador == "division");{
+        break;
+    
+   case "division":
         num1= parseInt(a);
         num2= parseInt(b)
         division=num1/num2;
         document.getElementById("resultDiv").innerHTML=division
+        break;
     }
    
-
-}
+     }
